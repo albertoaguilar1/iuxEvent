d62c27cf79bfc8ba97d6700ede8c832964190191
alberto.aguilar <faraond1@gmail.com>
2019-12-18 10:23:32 -0600

    asdad

--- index.js
+++ index.js
@@ -4,8 +4,7 @@
 var mongoose = require('mongoose');
 // *Cargamos el fichero app.js con la configuración de Express
 var app = require('./app');
-// Creamos la variable PORT para indicar el puerto en el que va a funcionar el servidor
-var port = 3800;
+
 // Le indicamos a Mongoose que haremos la conexión con Promesas
 mongoose.Promise = global.Promise;
 // Usamos el método connect para conectarnos a nuestra base de datos
